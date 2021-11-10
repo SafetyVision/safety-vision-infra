30903c5799a8b695ee3b2c7614c9f506a11270ce
Lewis Rafuse <lewisrafuse@gmail.com>
2021-11-09 23:24:00 -0500

    Lets encrypt setup.

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -5,11 +5,11 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(example.org www.example.org)
+domains=(safety-vision.ca)
 rsa_key_size=4096
 data_path="./data/certbot"
-email="" # Adding a valid address is strongly recommended
-staging=0 # Set to 1 if you're testing your setup to avoid hitting request limits
+email="rafusel@mcmaster.ca" # Adding a valid address is strongly recommended
+staging=1 # Set to 1 if you're testing your setup to avoid hitting request limits
 
 if [ -d "$data_path" ]; then
   read -p "Existing data found for $domains. Continue and replace existing certificate? (y/N) " decision
